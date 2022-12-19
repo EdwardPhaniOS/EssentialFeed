94c2b1ed3d7c9b2accbc2edf6821a9738bc28761
EdwardPhaniOS <EdwardPhan2015@gmail.com>
2022-12-19 17:40:00 +0700

    refactoring code to remove duplication

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -8,8 +8,16 @@
 import XCTest
 
 class RemoteFeedLoader {
+    let client: HTTPClient
+    let url: URL
+    
+    init(url: URL, client: HTTPClient) {
+        self.client = client
+        self.url = url
+    }
+    
     func load() {
-        HTTPClientSpy.shared.get(from: URL(string: "https://abcv.com.vn"))
+        client.get(from: url)
     }
 }
 
@@ -19,33 +27,36 @@ class HTTPClient {
     func get(from url: URL) { }
 }
 
-class HTTPClientSpy: HTTPClient {
-    
-    override func get(from url: URL) {
-        requestedURL = url
-    }
-    
-    var requestedURL: URL?
-    
-}
-
-
 final class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClientSpy()
-        HTTPClient.shared = client
-        let _ = RemoteFeedLoader()
+        let (_, client) = makeSUT()
         //Init RemoteFeedLoader does not request data
         XCTAssertNil(client.requestedURL)
     }
 
     func test_load_requestDataFromURL() {
-        let client = HTTPClientSpy()
-        HTTPClient.shared = client
-        let sut = RemoteFeedLoader()
-        
+        let url = URL(string: "https://abcv.com.vn")!
+        let (sut, client) = makeSUT(url: url)
+       
         sut.load()
-        XCTAssertNotNil(client.requestedURL)
+        
+        XCTAssertEqual(client.requestedURL, url)
+    }
+    
+    
+    //MARK: - Helpers
+    //
+    private func makeSUT(url: URL = URL(string: "https://abcv.com.vn")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        return (RemoteFeedLoader(url: url, client: client), client)
+    }
+    
+    private class HTTPClientSpy: HTTPClient {
+        var requestedURL: URL?
+        
+        override func get(from url: URL) {
+            requestedURL = url
+        }
     }
 }
