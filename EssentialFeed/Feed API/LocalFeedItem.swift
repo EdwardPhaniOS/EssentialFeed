09f47d5828866dab595163d064df80f1cfa98d85
EdwardPhaniOS <EdwardPhan2015@gmail.com>
2023-03-04 12:57:02 +0700

    Move Local Feed Item to its own file

---
+++ EssentialFeed/Feed API/LocalFeedItem.swift
@@ -0,0 +1,23 @@
+//
+//  LocalFeedItem.swift
+//  EssentialFeed
+//
+//  Created by Tan Vinh Phan on 04/03/2023.
+//
+
+import Foundation
+
+public struct LocalFeedItem: Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+    
+    public init(id: UUID, description: String? = nil, location: String? = nil, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+}
+
