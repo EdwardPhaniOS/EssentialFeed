4e598ee7f7877b07350c2e52d1c23df850e3e80d
EdwardPhaniOS <EdwardPhan2015@gmail.com>
2023-08-05 17:28:33 +0700

    Add localization tests to guarantee all localized keys have translations in all supported localizations

---
+++ EssentialFeediOSTests/Feed Presentation/FeedLocalizationTests.swift
@@ -0,0 +1,66 @@
+//
+//  FeedLocalizationTests.swift
+//  EssentialFeediOSTests
+//
+//  Created by Tan Vinh Phan on 05/08/2023.
+//
+
+import XCTest
+@testable import EssentialFeediOS
+
+final class FeedLocalizationTests: XCTestCase {
+
+    func test_localizedStrings_haveKeysAndValuesForAllSupportedLocalizations() {
+        let table = "Feed"
+        let presentationBundle = Bundle(for: FeedPresenter.self)
+        let localizationBundles = allLocalizationBundles(in: presentationBundle)
+        let localizedStringKeys = allLocalizedStringKeys(in: localizationBundles, table: table)
+        
+        localizationBundles.forEach { (bundle, languageCode) in
+            localizedStringKeys.forEach { key in
+                let localizedString = bundle.localizedString(forKey: key, value: nil, table: table)
+                
+                if localizedString == key {
+                    let language = Locale.current.localizedString(forLanguageCode: languageCode) ?? ""
+                    
+                    XCTFail("Missing \(language) \(languageCode) localized string for key: '\(key)' in table '\(table)'")
+                    
+                }
+            }
+        }
+    }
+    
+    
+    //MARK: - Helpers
+    
+    private typealias LocalizedBundle = (bundle: Bundle, languageCode: String)
+    
+    private func allLocalizationBundles(in bundle: Bundle, file: StaticString = #file, line: UInt = #line) -> [LocalizedBundle] {
+        return bundle.localizations.compactMap { languageCode in
+            guard
+                let path = bundle.path(forResource: languageCode, ofType: "lproj"),
+                let localizedBundle = Bundle(path: path)
+            else {
+                XCTFail("Couldn't find bundle for languageCode: \(languageCode)", file: file, line: line)
+                return nil
+            }
+            
+            return (localizedBundle, languageCode)
+        }
+    }
+    
+    private func allLocalizedStringKeys(in bundles: [LocalizedBundle], table: String, file: StaticString = #file, line: UInt = #line) -> Set<String> {
+        return bundles.reduce([]) { (acc, current) in
+            guard
+                let path = current.bundle.path(forResource: table, ofType: "strings"),
+                let strings = NSDictionary(contentsOfFile: path),
+                let keys = strings.allKeys as? [String]
+            else {
+                return acc
+            }
+            
+            return acc.union(Set(keys))
+        }
+    }
+
+}
