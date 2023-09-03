a27db8f92ba827d3e6e7c4be944701a9c2f7d262
EdwardPhaniOS <EdwardPhan2015@gmail.com>
2023-09-03 08:16:52 +0700

    Fix syntax error

--- EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
@@ -39,7 +39,7 @@ final class RemoteFeedImageDataLoaderTests: XCTestCase {
         let (sut, client) = makeSUT()
         let clientError = NSError(domain: "a client error", code: 0)
         
-        expect(sut, toCompleteWith: .failure(.connectivity)) {
+        expect(sut, toCompleteWith: .failure(RemoteFeedImageDataLoader.Error.connectivity)) {
             client.complete(with: clientError)
         }
     }
