0de98cdf0cba3ed2c25cf241bc21656cd158a81d
EdwardPhaniOS <EdwardPhan2015@gmail.com>
2023-10-07 11:27:54 +0700

    Display remote feed on launch when customer has connectivity

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -19,24 +19,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         guard let _ = (scene as? UIWindowScene) else { return }
         
-//        let remoteURL = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
-//        let session = URLSession(configuration: .ephemeral)
-//        let remoteClient = URLSessionHTTPClient(session: session)
-//        let remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: remoteClient)
-//        let remoteImageLoader = RemoteFeedImageDataLoader(client: remoteClient)
-//        
-//        let localStoreURL = NSPersistentContainer.defaultDirectoryURL().appendingPathComponent("feed-store.sqlite")
-//        let localStore = try! CoreDataFeedStore(storeURL: localStoreURL)
-//        let localFeedLoader = LocalFeedLoader(store: localStore) { Date() }
-//        let localImageLoader = LocalFeedImageDataLoader(store: localStore)
-//        
-//        window?.rootViewController = FeedUIComposer.feedComposeWith(
-//            feedLoader: FeedLoaderWithFallbackComposite(
-//                primary: localFeedLoader,
-//                fallback: remoteFeedLoader),
-//            imageLoader: FeedImageDataLoaderWithFallbackComposite(
-//                primary: localImageLoader,
-//                fallback: remoteImageLoader))
+        let remoteURL = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5db4155a4fbade21d17ecd28/1572083034355/essential_app_feed.json")!
+        
+        let remoteClient = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+        
+        let remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: remoteClient)
+        let remoteImageLoader = RemoteFeedImageDataLoader(client: remoteClient)
+        
+        window?.rootViewController = FeedUIComposer.feedComposeWith(feedLoader: remoteFeedLoader, imageLoader: remoteImageLoader)
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
