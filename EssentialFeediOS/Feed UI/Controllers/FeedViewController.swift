87004133a5d98f63cfb6bc9410b9953d0cb6ca79
EdwardPhaniOS <EdwardPhan2015@gmail.com>
2023-10-09 22:06:45 +0700

    Move FeedUI Composition details from EssentialFeediOS to the EssentialApp module (Composition Root))

--- EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import EssentialFeed
 
-protocol FeedViewControllerDelegate {
+public protocol FeedViewControllerDelegate {
     func didRequestFeedRequest()
 }
 
@@ -16,9 +16,9 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
     
     @IBOutlet weak var errorView: ErrorView!
     
-    var delegate: FeedViewControllerDelegate?
+    public var delegate: FeedViewControllerDelegate?
     
-    var tableModel = [FeedImageCellController]() {
+    private var tableModel = [FeedImageCellController]() {
         didSet {
             tableView.reloadData()
         }
@@ -31,6 +31,10 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
     
     @IBAction private func refresh() {
         delegate?.didRequestFeedRequest()
+    }
+    
+    public func display(_ cellControllers: [FeedImageCellController]) {
+        tableModel = cellControllers
     }
     
     public func display(_ viewModel: FeedLoadingViewModel) {
