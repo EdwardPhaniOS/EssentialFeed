83065aa6541a1d8754028f1c44b04c8f4184f6b3
EdwardPhan <EdwardPhan2015@gmail.com>
2024-02-12 20:00:18 +0700

    Add feed endpoint with "after_id" param

--- EssentialFeedAPITests/Shared API Infra/FeedEndpointTests.swift
+++ EssentialFeedAPITests/Shared API Infra/FeedEndpointTests.swift
@@ -6,14 +6,15 @@
 //
 
 import XCTest
+import EssentialFeed
 import EssentialFeedAPI
 
 final class FeedEndpointTests: XCTestCase {
-
+    
     func test_feed_endpointURL() {
         let baseURL = URL(string: "http://base-url.com")!
         
-        let received = FeedEndPoint.get.url(baseURL: baseURL)
+        let received = FeedEndPoint.get().url(baseURL: baseURL)
         let expected = URL(string: "http://base-url.com/v1/feed")!
         
         XCTAssertEqual(received.scheme, "http", "scheme")
@@ -21,5 +22,25 @@ final class FeedEndpointTests: XCTestCase {
         XCTAssertEqual(received.path, "/v1/feed", "path")
         XCTAssertEqual(received.query, "limit=10", "query")
     }
-
+    
+    func test_feed_endpointURLAfterGivenImage() {
+        let image = uniqueImage()
+        let baseURL = URL(string: "http://base-url.com")!
+        
+        let received = FeedEndPoint.get(after: image).url(baseURL: baseURL)
+        
+        XCTAssertEqual(received.scheme, "http", "scheme")
+        XCTAssertEqual(received.host, "base-url.com", "host")
+        XCTAssertEqual(received.path, "/v1/feed", "path")
+        XCTAssertEqual(received.query?.contains("limit=10"), true, "limit query params")
+        XCTAssertEqual(received.query?.contains("after_id=\(image.id.uuidString)"), true, "after_id query params")
+    }
+    
+    //MAKR - Helpers:
+    func uniqueImage() -> FeedImage {
+        return FeedImage(id: UUID(), description: "anyText", url: anyURL())
+    }
+    
 }
+
+
